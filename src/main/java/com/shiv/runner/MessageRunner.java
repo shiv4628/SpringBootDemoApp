52a9ef108e5f77ef85a4d249c1d1c84a5b9094a7
Usha <Usha@DESKTOP-9F58SDD>
2021-09-28 11:24:06 +0530

    WH-113 - next commit

--- src/main/java/com/shiv/runner/MessageRunner.java
+++ src/main/java/com/shiv/runner/MessageRunner.java
@@ -9,6 +9,7 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("welcome to springboot project");
+		System.out.println("welcome to 2nd springboot project");
 
 	}
 
