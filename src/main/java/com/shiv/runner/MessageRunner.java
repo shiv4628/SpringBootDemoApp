155dce478473e903580b8f2c46842f079bf2c596
Usha <Usha@DESKTOP-9F58SDD>
2021-09-28 09:57:02 +0530

    WH-1113 - first time update

---
+++ src/main/java/com/shiv/runner/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.shiv.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("welcome to springboot project");
+
+	}
+
+}
